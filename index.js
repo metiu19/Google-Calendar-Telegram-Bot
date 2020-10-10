2e9d558fe76ef9d8270ae1d2417936305d9dbc96
GdG_Metiu19 <metiu19programmer@gmail.com>
2020-10-10 18:10:43 +0200

    Fixed /compiti

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const Telegram = require('telegram-bot-api');
 const Tokens = require('./Tokens.json');
+const delay = require('delay');
+
+// Bot set up
+const Telegram = require('telegram-bot-api');
 const bot = new Telegram({token: Tokens.BotToken});
 const mp = new Telegram.GetUpdateMessageProvider();
 
@@ -10,6 +13,18 @@ bot.start()
     })
     .catch(err => console.error(err));
 
+// Google API set up
+const { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+const OAuth2Client = new OAuth2(Tokens.ClientId, Tokens.ClientSecret);
+
+OAuth2Client.setCredentials({
+    refresh_token: Tokens.RefreshToken
+});
+
+const calendar = google.calendar({ version: 'v3', auth: OAuth2Client });
+
+// Telegram bot update endler
 bot.on('update', update => {
     var message = update.message.text;
     if(message.startsWith('/')) {
@@ -40,27 +55,31 @@ bot.on('update', update => {
     }
 });
 
-const { google } = require('googleapis');
-const { OAuth2 } = google.auth;
-const OAuth2Client = new OAuth2(Tokens.ClientId, Tokens.ClientSecret);
-
-OAuth2Client.setCredentials({
-	refresh_token: Tokens.RefreshToken
-});
-
-const calendar = google.calendar({ version: 'v3', auth: OAuth2Client });
-
-function GetEvents(chatId) {
+// Get Calendar Events List
+function GetEvents(chatId) {    
+    const StartDate = new Date();
+    const EndDate = new Date();
+    EndDate.setDate(EndDate.getDay() + 12);
     calendar.calendarList.list({})
         .then(res => {
             const calendarId = res.data.items[0].id;
-            calendar.events.list({calendarId: calendarId})
+            calendar.events.list({
+                calendarId: calendarId,
+                orderBy: 'startTime',
+                singleEvents: true,
+                timeMax: EndDate,
+                timeMin: StartDate
+            })
             .then(events => {
+                var message = ``;
                 events.data.items.forEach(item => {
-                    bot.sendMessage({
-                        chat_id: chatId,
-                        text: `Materia: ${item.summary}\nCompiti: ${item.description}`
-                    });
+                    const dateTime = item.start.dateTime;
+                    var date = dateTime.slice(0, 10).split('-');
+                    message = message + `Materia: ${item.summary}\nCompito: ${item.description}\nPer: ${date[2]}/${date[1]}/${date[0]}\n\n`;
+                });
+                bot.sendMessage({
+                    chat_id: chatId,
+                    text: message
                 });
             })
             .catch(err => console.error('Get Event List error: ' + err));
